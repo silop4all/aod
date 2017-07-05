da02896a7b859483c28911b73241ecc0f4f7ebda
Panagiotis Athanasoulis <pathanasoulis@ep.singularlogic.eu>
2017-07-05 12:39:14 +0300

    Update th sql script that includes the pre-existing data in db

--- AssistanceOnDemand/sql/aod_data.sql
+++ AssistanceOnDemand/sql/aod_data.sql
@@ -73,8 +73,11 @@ INSERT INTO `app_components` (`id`, `name`, `description`, `description_el`, `de
 (3, 'subscription_banner', 'Activate or not the subscription banner.', 'Activate or not the subscription banner.', 'Activate or not the subscription banner.', 'Activate or not the subscription banner.', 'Activate or not the subscription banner.', 'Activate or not the subscription banner.', 'Activate or not the subscription banner.', 0),
 (4, 'add_to_cart', 'Cart mechanism', 'Cart mechanism', 'Cart mechanism', 'Cart mechanism', 'Cart mechanism', 'Cart mechanism', 'Cart mechanism', 0),
 (5, 'crowd_source_banner', 'Manage the integration among AoD infrastructure with Crowd source component.', 'Χερισμός της ενσωμάτωσης του social source μηχανισμού στο AoD.', 'Manage the integration among AoD infrastructure with Crowd source component.', 'Manage the integration among AoD infrastructure with Crowd source component.', 'Manage the integration among AoD infrastructure with Crowd source component.', 'Manage the integration among AoD infrastructure with Crowd source component.', 'Manage the integration among AoD infrastructure with Crowd source component.', 0),
-(6, 'ui_options_banner', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 1);
-
+(6, 'ui_options_banner', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 'The User Interface Options (UI Options) component allows users to transform the presentation of the user interface and content resources so that they are personalized to the individual user''s needs', 1),
+(7, 'microlabora_tasks_banner', 'Publish or not human tasks in the Microlabora in case that the required services are not available in the AoD.', 'Δυνατότηρα δημοσίευσης μίας υπηρεσίας που πραγματοποιείται από φυσικό πρόσωπο στο Microlabora σε περίπτωση που δεν υπάρχει στο AoD.', 'Publish or not human tasks in the Microlabora in case that the required services are not available in the AoD.', 'Publish or not human tasks in the Microlabora in case that the required services are not available in the AoD.', 'Publish or not human tasks in the Microlabora in case that the required services are not available in the AoD.', 'Publish or not human tasks in the Microlabora in case that the required services are not available in the AoD.', 'Publish or not human tasks in the Microlabora in case that the required services are not available in the AoD.', 0),
+(8, 'community_technical_support', 'Community technical support', 'Community technical support', 'Community technical support', 'Community technical support', 'Community technical support', 'Community technical support', 'Community technical support', 0),
+(9, 'try_different', 'Try different component', 'Try different component', 'Try different component', 'Try different component', 'Try different component', 'Try different component', 'Try different component', 0),
+(10, 'aod_community_support', 'Enable or not  the AoD community support', 'Enable or not  the AoD community support', 'Enable or not  the AoD community support', 'Enable or not  the AoD community support', 'Enable or not  the AoD community support', 'Enable or not  the AoD community support', 'Enable or not  the AoD community support', 0);
 
 
 
@@ -127,3 +130,65 @@ INSERT INTO `app_metadata` (`id`, `title`, `description`, `keywords`, `author`, 
 
 INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
 (1, 'pbkdf2_sha256$20000$bh3Ut5h1Sovy$e5oi17riuXoB6cz9JSeRKydNswpOtyQPj4oG0jsVhy4=', '2016-06-02 12:50:16.006000', 1, 'admin', '', '', 'admin@localhost.com', 1, 1, '2016-06-02 12:39:16.771000');
+
+
+
+INSERT INTO `ml_categories` (`id`, `title`, `title_de`, `title_el`, `title_en`, `title_es`, `title_fr`, `title_it`) VALUES
+(1, 'In-video/picture feature identification', NULL, NULL, 'In-video/picture feature identification', NULL, NULL, NULL),
+(2, 'Photographic retouch', NULL, NULL, 'Photographic retouch', NULL, NULL, NULL),
+(3, 'Photo and video capture', NULL, NULL, 'Photo and video capture', NULL, NULL, NULL),
+(4, 'Video edition and postproduction', NULL, NULL, 'Video edition and postproduction', NULL, NULL, NULL),
+(5, 'Sound mixing and mastering', NULL, NULL, 'Sound mixing and mastering', NULL, NULL, NULL),
+(6, 'Presentation/slide making from plain content', NULL, NULL, 'Presentation/slide making from plain content', NULL, NULL, NULL),
+(7, 'Video subtitling', NULL, NULL, 'Video subtitling', NULL, NULL, NULL),
+(8, 'Speech and dubbing', NULL, NULL, 'Speech and dubbing', NULL, NULL, NULL),
+(9, 'Music composition', NULL, NULL, 'Music composition', NULL, NULL, NULL),
+(10, 'Graphic design', NULL, NULL, 'Graphic design', NULL, NULL, NULL),
+(11, 'Illustration', NULL, NULL, 'Illustration', NULL, NULL, NULL),
+(12, 'Advertising design', NULL, NULL, 'Advertising design', NULL, NULL, NULL),
+(13, 'Banner design', NULL, NULL, 'Banner design', NULL, NULL, NULL),
+(14, 'Corporate logos and pictures', NULL, NULL, 'Corporate logos and pictures', NULL, NULL, NULL),
+(15, 'Animation', NULL, NULL, 'Animation', NULL, NULL, NULL),
+(16, 'Web design', NULL, NULL, 'Web design', NULL, NULL, NULL),
+(17, 'Polling', NULL, NULL, 'Polling', NULL, NULL, NULL),
+(18, 'Article writing and proofreading', NULL, NULL, 'Article writing and proofreading', NULL, NULL, NULL),
+(19, 'Web content creation', NULL, NULL, 'Web content creation', NULL, NULL, NULL),
+(20, 'Bibliographic research', NULL, NULL, 'Bibliographic research', NULL, NULL, NULL),
+(21, 'Data recording and typing', NULL, NULL, 'Data recording and typing', NULL, NULL, NULL),
+(22, 'Audio and text transcription', NULL, NULL, 'Audio and text transcription', NULL, NULL, NULL),
+(23, 'Translation', NULL, NULL, 'Translation', NULL, NULL, NULL),
+(24, 'Internet advertisement broadcasting', NULL, NULL, 'Internet advertisement broadcasting', NULL, NULL, NULL),
+(25, 'Data duplication', NULL, NULL, 'Data duplication', NULL, NULL, NULL),
+(26, 'Internet advertisement broadcasting (dupe)', NULL, NULL, 'Internet advertisement broadcasting (dupe)', NULL, NULL, NULL),
+(27, 'Web searching', NULL, NULL, 'Web searching', NULL, NULL, NULL),
+(28, 'Banner creation', NULL, NULL, 'Banner creation', NULL, NULL, NULL),
+(29, 'Server administration', NULL, NULL, 'Server administration', NULL, NULL, NULL),
+(30, 'Technical support and troubleshooting with specific equipment and software', NULL, NULL, 'Technical support and troubleshooting with specific equipment and software', NULL, NULL, NULL),
+(31, 'Remote software installation', NULL, NULL, 'Remote software installation', NULL, NULL, NULL),
+(32, 'Technical support', NULL, NULL, 'Technical support', NULL, NULL, NULL),
+(33, 'Design', NULL, NULL, 'Design', NULL, NULL, NULL),
+(34, 'Programming', NULL, NULL, 'Programming', NULL, NULL, NULL),
+(35, 'Web analysis', NULL, NULL, 'Web analysis', NULL, NULL, NULL),
+(36, 'System analysis', NULL, NULL, 'System analysis', NULL, NULL, NULL),
+(37, 'Development analysis', NULL, NULL, 'Development analysis', NULL, NULL, NULL),
+(38, 'Polling', NULL, NULL, 'Polling', NULL, NULL, NULL),
+(39, 'Internet advertisement broadcasting (dupe)', NULL, NULL, 'Internet advertisement broadcasting (dupe)', NULL, NULL, NULL),
+(40, 'Viral marketing and advertising campaigns', NULL, NULL, 'Viral marketing and advertising campaigns', NULL, NULL, NULL),
+(41, 'Teleoperator', NULL, NULL, 'Teleoperator', NULL, NULL, NULL),
+(42, 'Web searching (dupe)', NULL, NULL, 'Web searching (dupe)', NULL, NULL, NULL),
+(43, 'Trend analysis', NULL, NULL, 'Trend analysis', NULL, NULL, NULL),
+(44, 'Analista publicitario', NULL, NULL, 'Analista publicitario', NULL, NULL, NULL),
+(45, 'Marketing analysis', NULL, NULL, 'Marketing analysis', NULL, NULL, NULL),
+(46, 'Business and market research', NULL, NULL, 'Business and market research', NULL, NULL, NULL),
+(47, 'Accesibility consulting', NULL, NULL, 'Accesibility consulting', NULL, NULL, NULL),
+(48, 'Financial analysis', NULL, NULL, 'Financial analysis', NULL, NULL, NULL),
+(49, 'Inteligence analysis', NULL, NULL, 'Inteligence analysis', NULL, NULL, NULL),
+(50, 'Risk analysis', NULL, NULL, 'Risk analysis', NULL, NULL, NULL),
+(51, 'Technology analysis', NULL, NULL, 'Technology analysis', NULL, NULL, NULL);
+
+
+insert into `app_evaluation_metrics` values 
+(1, 'Usability', 'Measures the easiness a user can experience while he uses the service', 1, 0, 5, 1),
+(2, 'Usefulness', 'Measures how much useful is the service', 1, 0, 5, 1),
+(3, 'Effectiveness', 'Measures the degree that the service satisfies the needs of user', 1, 0, 5, 1),
+(4, 'Availability', 'Measures how much available is the service during a day', 1, 0, 5, 1);
